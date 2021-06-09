dfd8d52cf8b796c9219601a41e50f84543ac68a7
Reinaldo Andres Juares Acevedo <rjuaresacevedo@gmail.com>
2021-06-09 17:09:14 -0300

    Se crearon las clases de la clase 2

---
+++ Complejidad2021Github/ComplejidadTemporal/ComplejidadTemporal/ArbolBinario.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ComplejidadTemporal
+{
+    public class ArbolBinario<T>
+    {
+
+        //VARIABLES QUE TENDRA LA CLASE ARBOLBINARIO
+        private T dato;
+        private ArbolBinario<T> Hijoizquierdo;
+        private ArbolBinario<T> Hijoderecho;
+
+
+        //CONSTRUCTOR VACIO
+        public ArbolBinario()
+        {
+        }
+
+        //CONSTRUCTOR CON PARAMETRO
+        public ArbolBinario(T dato) {
+            this.dato = dato;
+        }
+
+        //METODO PARA OBTENER EL DATO DE LA RAIZ
+        public T GetDatoRaiz(){
+            return this.dato;
+        }
+
+        //METODO PARA AGREGAR EL HIJO IZQUIERDO
+        public void agregarHijoIzq(ArbolBinario<T> Hijoizq){
+            this.Hijoizquierdo = Hijoizq;
+        }
+
+        //METODO PARA AGREGAR EL HIJO DERECHO
+        public void agregarHijoDer(ArbolBinario<T> HijoDer){
+            this.Hijoderecho = HijoDer;
+        }
+
+        //METODO PARA ELIMINAR EL HIJO IZQUIERDO
+        public void eliminarHijoIzq(){
+            this.Hijoizquierdo = null;
+        }
+
+        //METODO PARA ELIMINAR EL HIJO DERECHO
+        public void eliminarHijoDer(){
+            this.Hijoderecho = null;
+        }
+
+        //METODO PARA SABER SI ES HOJA
+        public bool EsHoja() {
+            return this.Hijoizquierdo == null && this.Hijoderecho == null;
+        }
+
+        //METODO PARA RECORRIDO EN INORDEN
+        public void Inorden() {
+            
+        }
+
+        //METODO PARA RECORRIDO EN POSTORDEN
+        public void PostOrden() { 
+        
+        }
+
+        //METODO PARA RECORRIDO EN PREORDEN
+        public void Preorden() { 
+        
+        }
+
+
+        //METODO PARA RECORRIDO POR NIVELES
+
+        //METODO PARA CONTAR HOJAS 
+        public int ContarHojas() {
+            return 0;
+        }
+        //METODO PARA RECORRER ENTRE NIVELES
+        public void RecorridoEntreNiveles(){
+        
+        }
+    }
+}
--- Complejidad2021Github/ComplejidadTemporal/ComplejidadTemporal/Program.cs
+++ Complejidad2021Github/ComplejidadTemporal/ComplejidadTemporal/Program.cs
@@ -6,6 +6,23 @@ namespace ComplejidadTemporal
     {
         public static void Main(string[] args)
         {
+            ArbolBinario<int> arbolBinarioA = new ArbolBinario<int>(1);
+
+            ArbolBinario<int> hijoIzquierdo = new ArbolBinario<int>(2);
+            hijoIzquierdo.agregarHijoIzq(new ArbolBinario<int>(3));
+            hijoIzquierdo.agregarHijoDer(new ArbolBinario<int>(4));
+
+            ArbolBinario<int> hijoDerecho = new ArbolBinario<int>(5);
+            hijoDerecho.agregarHijoIzq(new ArbolBinario<int>(6));
+            hijoDerecho.agregarHijoDer(new ArbolBinario<int>(7));
+
+            arbolBinarioA.agregarHijoIzq(hijoIzquierdo);
+            arbolBinarioA.agregarHijoDer(hijoDerecho);
+
+            arbolBinarioA.Preorden();
+            arbolBinarioA.Inorden();
+            arbolBinarioA.PostOrden();
+            arbolBinarioA.RecorridoEntreNiveles();
             Console.WriteLine("Hola Mundo");
         }
     }

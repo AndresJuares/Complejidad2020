736e04f7eb023b5905de835562b2000ae7c207ed
Reinaldo Andres Juares Acevedo <rjuaresacevedo@gmail.com>
2021-06-09 14:49:34 -0300

    Se subieron clases nuevas a la carpeta

---
+++ Complejidad2021Github/ComplejidadTemporal/ComplejidadTemporal/ArbolGeneral.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ComplejidadTemporal
+{
+    public class ArbolGeneral<T>
+    {
+        //VARIABLES QUE TENDRA LA CLASE
+        private T dato;
+        private List<ArbolGeneral<T>> hijos = new List<ArbolGeneral<T>>();
+
+
+        //CONSTRUCTOR CON PARAMETRO
+        public ArbolGeneral(T dato)
+        {
+            this.dato = dato;
+        }
+
+        //METODO PARA OBTENER EL DATO DE LA RAIZ
+        public T GetDatoRaiz()
+        {
+            return this.dato;
+        }
+
+
+        //METODO PARA OBTENER UN HIJO
+        public List<ArbolGeneral<T>> GetHijos()
+        {
+            return hijos;
+        }
+
+
+        //METODO PARA AGREGAR UN HIJO
+        public void AgregarHijo(ArbolGeneral<T> Hijo)
+        {
+            GetHijos().Add(Hijo);
+        }
+
+        //METODO PARA ELIMINAR UN HIJO
+        public void EliminarHijo(ArbolGeneral<T> Hijo)
+        {
+            this.GetHijos().Remove(Hijo);
+
+        }
+
+        //METODO PARA SABER SI ES HOJA
+
+        public bool EsHoja()
+        {
+            return this.GetHijos().Count == 0;
+        }
+
+
+        //METODO PARA SABER LA ALTURA
+        public int altura()
+        {
+            return 0;
+        }
+
+        //METODO PARA SABER EL NIVEL
+        public int nivel(T dato)
+        {
+            return 0;
+        }
+    }
+}
---
+++ Complejidad2021Github/ComplejidadTemporal/ComplejidadTemporal/Cola.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ComplejidadTemporal
+{
+    public class Cola<T>
+    {
+        //VARIABLES QUE TENDRA LA CLASE COLA
+        private List<T> datos = new List<T>();
+
+
+        //CONSTRUCTOR DE LA CLASE
+        public Cola()
+        {
+        }
+
+        //METODO PARA ENCOLAR
+        public void encolar(T elem)
+        {
+            this.datos.Add(elem);
+        }
+
+
+        //METODO PARA DESENCOLAR
+        public T desencolar()
+        {
+            T temp = this.datos[0];
+            this.datos.RemoveAt(0);
+            return temp;
+        }
+
+        //METODO PARA SABER EL TOPE
+        public T tope()
+        {
+            return this.datos[this.datos.Count - 1];
+        }
+
+        //METODO PARA SABER SI LA COLA ESTA VACIA
+        public bool EsVacia()
+        {
+            return this.datos.Count == 0;
+        }
+    }
+}
